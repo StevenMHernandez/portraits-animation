74112f8496651c69906d33beaa2e0f5a96b04b52
StevenmHernandez <steven@coolertags.com>
2014-11-22 14:18:57 -0500

    counter added for image name (TODO integrate db)

--- app.js
+++ app.js
@@ -8,48 +8,83 @@ var util = require('util');
 var fs = require('fs-extra');
 var gm = require('gm');
 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(':memory:');
+//TODO change from :memory: ??
+
+//db.serialize(function () {
+//    db.run("CREATE TABLE lorem (info TEXT)");
+//
+//    var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+//    for (var i = 0; i < 10; i++) {
+//        stmt.run("dd " + i);
+//    }
+//    stmt.finalize();
+//
+//    db.each("SELECT rowid AS id, info FROM lorem", function (err, row) {
+//        console.log(row.id + ": " + row.info);
+//    });
+//});
+//
+//db.close();
+var imageCounter = 0;
+//TODO replace with db count
+
 server.listen(3003);
 var new_location = 'uploads/';
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
 app.get('/', function (req, res) {
+
     res.sendfile(__dirname + '/views/index.html');
 });
 
 app.get('/animation', function (req, res) {
     res.sendfile(__dirname + '/views/animation.html');
+    //TODO add counter number some how.
 });
 
 app.post('/upload', function (req, res) {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
         var temp_path = files['upload'].path;
-        var file_name = files['upload'].name;
+        var file_type = files['upload'].type;
 
-        gm(temp_path).size(function (err, value) {
-            console.log(value);
-            var max, min;
-            value.width > value.height ? (max=value.width, min=value.height) : (max=value.height, min=value.width);
-
-
-            console.log((value.width - min)/2);
-            console.log((value.height - min)/2);
-            console.log(min +'.'+ max);
-            gm(temp_path)
-                .crop(min, min , Math.round((value.width - min)/2), Math.round((value.height - min)/2))
-                .sample(999)
-                .resize(450, 450)
-                .write(new_location + file_name, function (err) {
-                    if (err) {
-                        console.error(err);
-                    } else {
-                        console.log(new_location + file_name);
-                        res.sendfile(new_location + files['upload']['name']);
-                        animation.emit('news', {image: new_location + files['upload']['name']})
+        if (file_type == 'image/jpeg' || file_type == 'image/jpg' || file_type == 'image/png') {
+            if (file_type == 'image/jpeg' || file_type == 'image/jpg') {
+                file_type = '.jpg';
+            }
+            else {
+                file_type = '.png';
+            }
+            gm(temp_path).size(function (err, value) {
+                console.log(value);
+                var max, min;
+                value.width > value.height ? (max = value.width, min = value.height) : (max = value.height, min = value.width);
+                //console.log((value.width - min) / 2);
+                //console.log((value.height - min) / 2);
+                //console.log(min + '.' + max);
+                gm(temp_path)
+                    .crop(min, min, Math.round((value.width - min) / 2), Math.round((value.height - min) / 2))
+                    .sample(999)
+                    .resize(450, 450)
+                    .write(new_location + imageCounter + file_type, function (err) {
+                        if (err) {
+                            console.error(err);
+                        } else {
+                            imageCounter++;
+                            console.log(files['upload']);
+                            res.sendfile(new_location + imageCounter + file_type);
+                            animation.emit('news', {image: new_location + files['upload']['name']})
+                        }
                     }
-                }
-            );
-        });
+                );
+            });
+        }
+        else {
+            res.end('only jpeg, jpg and png');
+            //TODO send back to /
+        }
     });
 });
 
