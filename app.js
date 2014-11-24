9cbc7dd298146cf725fb3c821e641547ee4fac1c
StevenmHernandez <steven@coolertags.com>
2014-11-23 19:29:28 -0500

    Images = all jpg. animation does not query server even n seconds

--- app.js
+++ app.js
@@ -43,11 +43,6 @@ app.post('/upload', function (req, res) {
             var temp_path = files['upload'].path;
             var file_type = files['upload'].type;
             if (file_type == 'image/jpeg' || file_type == 'image/jpg' || file_type == 'image/png') {
-                if (file_type == 'image/jpeg' || file_type == 'image/jpg') {
-                    file_type = '.jpg';
-                } else {
-                    file_type = '.png';
-                }
                 gm(temp_path).size(function (err, value) {
                     if (err) {
                         console.log('gm::' + err);
@@ -58,19 +53,18 @@ app.post('/upload', function (req, res) {
                             .crop(min, min, Math.round((value.width - min) / 2), Math.round((value.height - min) / 2))
                             .sample(999)
                             .resize(450, 450)
-                            //.setFormat("jpg")
-                            .write(new_location + imageCounter + file_type, function (err) {
+                            .setFormat("jpg")
+                            .write(new_location + imageCounter + '.jpg', function (err) {
                                 if (err) {
                                     console.error(err);
                                 } else {
-                                    res.sendfile(new_location + imageCounter + file_type);
-                                    animation.emit('newImage', {image: new_location + imageCounter + file_type});
+                                    res.sendfile(new_location + imageCounter + '.jpg');
+                                    animation.emit('newImage', {uri: new_location + imageCounter + '.jpg', imageCount: imageCounter});
                                     //add to db
                                     db.serialize(function () {
                                         var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-                                        db.run("INSERT INTO images ('uri', 'ip') VALUES ('" + new_location + imageCounter + file_type + "','" + ip + "')");
+                                        db.run("INSERT INTO images ('uri', 'ip') VALUES ('" + new_location + imageCounter + ".jpg','" + ip + "')");
                                     });
-                                    //TODO no need to send this information though. It could be image: "added"
                                     imageCounter++;
                                     //TODO send file to
                                 }
@@ -89,18 +83,8 @@ app.post('/upload', function (req, res) {
 
 var animation = io.of('/animation');
 io.on('connection', function (socket) {
-    console.log('There were ' + imageCounter + ' images already available in the database.');
     socket.on('getImageCount', function () {
         console.log('getImageCount ran');
         socket.emit('imageCount', {imageCount: imageCounter});
-    });
-    socket.on('getRandom', function (data) {
-        db.each("SELECT * FROM images WHERE id=" + Math.round(Math.random()*imageCounter), function (err, row) {
-            animation.emit('newImage', {id: row.id, uri: row.uri});
-        });
-    });
-
-    socket.on('connection', function (socket) {
-        console.log('someone connected');
     });
 });
