dbc3c3953887fcefa7a3e75fabd8a926b7e2b678
StevenmHernandez <steven@coolertags.com>
2014-11-25 09:08:57 -0500

    seperated javascript

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -8,3 +8,17 @@ setInterval(function () {
     var width = Math.random() * (450 - x);
     ctx.drawImage(img,x,0,width,450,x+(Math.random()*99)-45,0,width+(Math.random()*99)-45,450);
 }, 9);
+
+function getRandom() {
+    var random = Math.floor(Math.random() * imageCount);
+    console.log(imageCount, random);
+    if (random >= 0) {
+        placeImage('uploads/' + random + '.jpg');
+    }
+}
+
+function placeImage(uri) {
+    if (uri) {
+        $('img:eq(' + Math.round(Math.random() * 9) + ')').attr('src', '/' + uri);
+    }
+}
